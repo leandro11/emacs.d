8b3acf98c7ba5d426f59236bb32b2f0c74f51d3d
Tao <taoluo@combitel.com.au>
2016-04-13 14:18:40 +1000

    add magit

--- init.el
+++ init.el
@@ -173,3 +173,10 @@
 
 (setq debug-on-error t)
 
+;; MAGIT
+(add-to-list 'load-path "~/.emacs.d/site-lisp/magit/lisp")
+(require 'magit)
+(with-eval-after-load 'info
+  (info-initialize)
+  (add-to-list 'Info-directory-list
+	       "~/.emacs.d/site-lisp/magit/Documentation/"))
